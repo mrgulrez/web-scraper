825e3db951f901744a1cf7dea45d5b1239b78df9
Gulrez Alam <egulrezalam@gmail.com>
2024-05-30 02:36:53 +0530

    chore: Remove unnecessary admin URL from scrapper/urls.py and update view_data.html template with Tailwind CSS classes

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import extract_data, view_data
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', extract_data, name='extract_data'),
     path('view/<int:data_id>/', view_data, name='view_data'),
 ]
